d110cc9685042c1d5451af8a04906adf83f6ffc6
Federico Ripley <61242689+omgitsfrederick@users.noreply.github.com>
2022-02-15 13:07:40 +0000

    First commit

---
+++ Source/EvolutionCycle/EvolutionCycle.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EvolutionCycle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EvolutionCycle, "EvolutionCycle" );
---
+++ Source/EvolutionCycle/EvolutionCycleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EvolutionCycleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVOLUTIONCYCLE_API AEvolutionCycleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
