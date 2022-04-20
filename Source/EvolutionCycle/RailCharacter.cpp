b39651a40b29ce1563f03432e59df59fc794b335
1903380 <1903380@uad.ac.uk>
2022-04-20 13:58:41 +0100

    Merge branch 'Character-Controller' into staging

---
+++ Source/EvolutionCycle/RailCharacter.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RailCharacter.h"
+
+// Sets default values
+ARailCharacter::ARailCharacter()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	// Set variables
+	Speed = 0;
+	IsBikeInputEnabled = 0;
+	DistanceCovered = 0;
+}
+
+// Called when the game starts or when spawned
+void ARailCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARailCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ARailCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+}
+
+void ARailCharacter::CharacterMovement(float DeltaTime)
+{
+	if (Speed > 0)
+	{
+		//Adjust to spline
+		FVector NewLocation;
+		FVector SplineLocation = CurrentSpline->FindLocationClosestToWorldLocation(GetActorLocation(), ESplineCoordinateSpace::World);
+		if (GetActorLocation().Equals(SplineLocation, 0.05))
+		{
+			NewLocation.X = SplineLocation.X;
+			NewLocation.Y = SplineLocation.Y;
+			NewLocation.Z = GetActorLocation().Z;
+			SetActorLocation(NewLocation);
+		}
+
+		//Move along forward vector
+		NewLocation = GetActorLocation() + (GetActorForwardVector() * Speed * DeltaTime);
+		SetActorLocation(NewLocation);
+
+		// Rotate to spline
+		FVector NewRotation = CurrentSpline->FindDirectionClosestToWorldLocation(GetActorLocation(), ESplineCoordinateSpace::World);
+		SetActorRelativeRotation(NewRotation.Rotation());
+
+		// Speed Falloff
+		Speed -= (Speed / 400) + 0.5;
+		
+		// Calculate distance covered
+		DistanceCovered = CurrentSpline->GetDistanceAlongSplineAtSplineInputKey(CurrentSpline->FindInputKeyClosestToWorldLocation(GetActorLocation()));
+	}
+}
---
+++ Source/EvolutionCycle/RailCharacter.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/SplineComponent.h"
+#include "RailCharacter.generated.h"
+
+UCLASS()
+class EVOLUTIONCYCLE_API ARailCharacter : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ARailCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Character components
+	UPROPERTY(BlueprintReadWrite)
+	USplineComponent* CurrentSpline;
+
+	// Character variables
+	UPROPERTY(BlueprintReadWrite)
+		float DistanceCovered;
+	UPROPERTY(BlueprintReadWrite)
+	float Speed;
+	UPROPERTY(BlueprintReadWrite)
+	bool IsBikeInputEnabled;
+
+	UFUNCTION(BlueprintCallable, Category = "RailCharacter")
+	void CharacterMovement(float DeltaTime);
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+};
